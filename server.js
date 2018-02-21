25f1661581f64540d67bbbe23371c84b38858f7c
rishiiii <36667124+rishiiii@users.noreply.github.com>
2018-02-21 16:17:56 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,6 +9,18 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
+app.get('/artical-one',function (req, res){
+   res.send('Artical one is requested from the server'); 
+});
+
+app.get('/artical-two',function (req, res){
+   res.send('Artical two is requested from the server'); 
+});
+
+app.get('/artical-three',function (req, res){
+   res.send('Artical three is requested from the server'); 
+});
+
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
